41e1398d32cec62f029a13afc3d23f3faf9037ee
manupor <135657852+manupor@users.noreply.github.com>
2026-03-28 08:56:11 -0600

    fix: Remove 10+ years badge and phone section from AboutSec2

--- src/features/home-two/AboutSec2.tsx
+++ src/features/home-two/AboutSec2.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from 'react-i18next';
 import AboutM from '@/images/consultants-img-06.jpg';
 import Af1 from '@/images/af-1.png';
 import Af2 from '@/images/af-2.png';
-import CounterUp from '@/components/elements/CounterUp';
 import TextAnimation from '@/components/elements/TextAnimation';
 import FadeInAdvanced from '@/components/elements/FadeInAdvanced';
 
@@ -18,24 +17,7 @@ export default function AboutSec2() {
                   {/* Left column - LogiXpress style: large image + years badge bottom-right */}
                   <div className="col-lg-6">
                      <FadeInAdvanced variant='fadeInLeft'>
-                        <div style={{ position: 'relative' }}>
-                           <div style={{
-                              position: 'absolute',
-                              bottom: '20px',
-                              right: '20px',
-                              zIndex: 2,
-                              backgroundColor: '#F7941D',
-                              color: 'white',
-                              padding: '24px 28px',
-                              borderRadius: '8px',
-                              textAlign: 'center',
-                              minWidth: '160px'
-                           }}>
-                              <div style={{ fontSize: '52px', fontWeight: 'bold', lineHeight: 1, marginBottom: '6px' }}>
-                                 <CounterUp ending={10} />+
-                              </div>
-                              <div style={{ fontSize: '14px', lineHeight: '1.5' }}>{t('about2.yearsLabel')}</div>
-                           </div>
+                        <div>
                            <img src={AboutM} className="img-fluid w-100" alt="HiHub Global Operations" style={{ borderRadius: '8px', objectFit: 'cover', maxHeight: '500px' }} />
                         </div>
                      </FadeInAdvanced>
@@ -59,15 +41,6 @@ export default function AboutSec2() {
                         </ul>
                         <div className="d-sm-flex align-items-center about-cta gap-5">
                            <Link to="/about" className="btn btn-primary">{t('about2.btn')} <i className="fa fa-arrow-right"></i><span style={{ top: '267.45px', left: '119.6px' }}></span> </Link>
-
-                           <div className="quick-call d-flex align-items-center">
-                              <span className="bg-dark icon-lg rounded-circle text-info"><i
-                                 className="fa-solid fa-phone-volume"></i></span>
-                              <div className="conn-txt ms-3">
-                                 <p className="m-0" style={{ color: '#ffffff' }}>{t('about2.callLabel')}</p>
-                                 <a className="h6 text-primary" href="tel:148359505285">148 359 505 285</a>
-                              </div>
-                           </div>
                         </div>
                      </div>
                      <div className="about-footer d-sm-flex align-items-center gap-5 bg-shade round mt-5">
